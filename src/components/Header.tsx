6e1c798bf2606f5c97bbbe890b0b50e2e7a04280
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-10-19 14:53:14 -0500

    feat: added spashScree css

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+const Header = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
---
+++ src/components/Splash.styles.ts
@@ -0,0 +1,13 @@
+import {Animated, StyleSheet} from 'react-native';
+
+const stylesSplash = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  animateView: {
+    backgroundColor: '#61dafb',
+  },
+});
