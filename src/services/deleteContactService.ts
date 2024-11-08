4e0a55eba3ef07e4e99326a17820c306c17b5b75
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-11-07 20:04:44 -0500

    refactor: conect delete with backen

--- src/services/deleteContactService.ts
+++ src/services/deleteContactService.ts
@@ -1,12 +1,22 @@
 import {API_URL} from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 export const deleteContactService = async (
   contactId: number,
 ): Promise<void> => {
   try {
-    //coment
-    await axios.delete(`${API_URL}/contacts/${contactId}`);
+    const token = await AsyncStorage.getItem('accessToken');
+
+    if (!token) {
+      console.error('No token found');
+      return;
+    }
+    await axios.delete(`${API_URL}/api/contacts/${contactId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     console.log(`Contact with id ${contactId} deleted successfully`);
   } catch (error) {
     console.error('Error deleting contact:', error);
